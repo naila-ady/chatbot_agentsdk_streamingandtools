2bb818f536d4c0e0dceee3f130b35713ab82b6d2
naila-ady <164941279+naila-ady@users.noreply.github.com>
2025-05-30 18:44:53 +0500

    created chatbot  giving response using LLM

---
+++ chatbot.py
@@ -0,0 +1,11 @@
+import chainlit as cl
+
+cl.on_message
+async def handle_message(message:cl.Message):
+    await cl.Message(
+        content=f"Received: {message.content}",
+    ).send()
+
+
+
+
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from chatbot-agentsdk-streamingandtools!")
+
+
+if __name__ == "__main__":
+    main()
